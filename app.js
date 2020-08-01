b1df0a5f45fc2ee0204d73e614a2067e834a0582
Souvik <souvikde.ns@gmail.com>
2020-08-01 21:35:58 +0530

    created server file

--- app.js
+++ app.js
@@ -7,6 +7,4 @@ app.get('/', (req,res) => {
     return res.send('Server')
 })
 
-app.listen(process.env.PORT || 5000,() => {
-    console.log(`Server started on port ${process.env.PORT || 5000}`)
-})
+module.exports = app
