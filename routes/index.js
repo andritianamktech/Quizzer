3c04f469c42997abce01bd2d8093c2263dd74672
Souvik <souvikde.ns@gmail.com>
2020-08-03 18:47:49 +0530

    connected quizzes route 😎

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const authRoute = require('./auth')
+const quizRoute = require('./quiz')
 
 router.use('/auth', authRoute)
+router.use('/quizzes', quizRoute)
 
 
 module.exports = router
