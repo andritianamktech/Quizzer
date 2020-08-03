0e2e33529a618a41609e4b7d1678ff1bfd4670fd
Souvik <souvikde.ns@gmail.com>
2020-08-03 10:11:20 +0530

    creating pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom'
 
+// importing pages
+import Index from './pages/index'
+import CreateQuiz from "./pages/create.quiz";
 
 function App() {
-  return (
-    <div>
+    return (
+        <div>
 
-    </div>
-  );
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={'/'} component={Index}/>
+                    <Route exact path={'/create'} component={CreateQuiz}/>
+                </Switch>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/pages/create.quiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateQuiz = props => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CreateQuiz
