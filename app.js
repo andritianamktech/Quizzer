198cd240ca2dea0d94871e20a048470775decda8
Souvik <souvikde.ns@gmail.com>
2020-08-03 08:54:17 +0530

    Updated build script 🎉

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
 
 
 const app = express()
+app.use(express.json())
+app.use(express.raw())
 
-app.get('/', (req,res) => {
-    return res.send('Server')
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+
+// client handling
+app.get('*',(req,res) => {
+  res.sendFile(path.join(__dirname,'client','build','index.html'))
 })
 
 module.exports = app
