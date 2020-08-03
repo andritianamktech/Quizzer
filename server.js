121f8970890801c9d64c352ec6d15bdefaa2302d
Souvik <souvikde.ns@gmail.com>
2020-08-03 09:17:11 +0530

    mongoose connected 😀

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
 
-app.listen(process.env.PORT || 5000, () => {
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log(`Server started on port ${process.env.PORT || 5000}`)
+  })
+}).catch(err => {
+  console.log(err)
 })
