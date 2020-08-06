8012015391f56eed40f1d4b1b5f09889cddc6b7d
Souvik <souvikde.ns@gmail.com>
2020-08-06 08:41:24 +0530

    Created passport middleware

---
+++ passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./model/users')
+const passportJWT = require('passport-jwt')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, async (username, password, cb) => {
+    try {
+        let user = await User.findOne({username: username})
+
+        if (!user) return cb(null, false, { message: 'Incorrect username' })
+
+        await user.comparePassword(password, (err, isMatch) => {
+            if(err) return cb(err)
+            if (!isMatch) return cb(null, false, { message: 'Incorrect password' })
+
+            return cb(null, user, { message: 'Login successfull' })
+        })
+
+    } catch (error) {
+        console.log(error)
+        cb(error)
+    }
+}))
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+}, async (jwtPayload, cb) => {
+    try {
+        const user = await User.findOne({ _id: jwtPayload.id })
+        cb(null, user)
+    }
+    catch (err) {
+        cb(err)
+    }
+}))
