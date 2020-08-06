3474afae522f8ec7d74af8d3fdb5bc5450eaccb5
Souvik <souvikde.ns@gmail.com>
2020-08-06 08:50:23 +0530

    created login and signup routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,43 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const User = require('../model/users')
 
+router.route('./login')
+    .post(async (req, res) => {
+        let user = req.user
+
+        try {
+            let token = jwt.sign({id: user._id}, process.env.JWT_TOKEN)
+            return res.status(200).json({token: token, username: user.username})
+        } catch (e) {
+            return res.status(404).json(e)
+        }
+    })
+
+router.route('./signup')
+    .post(async (req, res) => {
+        const {username, password} = req.body
+
+        try {
+            const user = await User.findOne({username: username})
+
+            if (user) {
+                return res.status(401).json({message: "username already exists"})
+            }
+
+            const newUser = new User({
+                username: username,
+                password: password
+            })
+
+            const response = await newUser.save()
+
+            const token = jwt.sign({id: response._id}, process.env.JWT_SECRET)
+
+            return res.status(201).json({token, username: response.username})
+        } catch (e) {
+            return res.status(404).json(e)
+        }
+    })
 
 module.exports = router
