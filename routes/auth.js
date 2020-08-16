0e5a315c75e8f12e83b4d9b6dac1fed5e240921d
rohitjethoe <rohitchai@icloud.com>
2020-08-16 18:19:22 +0200

    Created a module instead of seperate functions.

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,13 @@
-import { userLogin, userSignUp } from '../controller/auth';
-
+const authController = require('../controller/auth');
 const router = require('express').Router()
 const passport = require('passport')
 
 // Login route
 router.route('/login')
-    .post(passport.authenticate('local',{session: false}), async (req, res) => await userLogin(req, res));
+    .post(passport.authenticate('local',{session: false}), async (req, res) => await authController.userLogin(req, res));
 
 // Signup route
 router.route('/signup')
-    .post(async (req, res) => await userSignUp(req, res));
+    .post(async (req, res) => await authController.userSignUp(req, res));
 
 module.exports = router
