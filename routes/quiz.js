793a31f789984cfdd43c8f98d12474c8ea7c446b
rohitjethoe <rohitchai@icloud.com>
2020-08-16 18:17:16 +0200

    Created a module instead of seperate functions.

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,19 +1,16 @@
-import { createQuiz, createWorksheet, fetchWorksheet } from '../controller/quiz'
- 
-const router = require('express').Router()
-const { Quiz } = require('../model/quiz')
-const passport = require('passport')
-const { Worksheet } = require('../model/worksheet')
+const quizController = require('../controller/quiz');
+const router = require('express').Router();
+const passport = require('passport');
 
 // Create single quiz route.
 router.route('/quiz')
-    .post(async (req, res) => await createQuiz(req, res));
+    .post(async (req, res) => await quizController.createQuiz(req, res));
 
 // Create single worksheet route.
 router.route('/worksheet')
-    .post(passport.authenticate('jwt'), async (req, res) => await createWorksheet(req, res));
+    .post(passport.authenticate('jwt'), async (req, res) => await quizController.createWorksheet(req, res));
 
 router.route('/worksheet/:id')
-    .get(async (req, res) => await fetchWorksheet(req, res));
+    .get(async (req, res) => await quizController.fetchWorksheet(req, res));
 
-module.exports = router
+module.exports = router;
