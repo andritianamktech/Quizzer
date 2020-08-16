cdb6a96df576775daaac4de1a043e34e5b7cfbf6
rohitjethoe <rohitchai@icloud.com>
2020-08-16 17:23:22 +0200

    Added Authentication Controller for login and signup.

--- routes/auth.js
+++ routes/auth.js
@@ -1,47 +1,14 @@
+import { userLogin, userSignUp } from '../controller/auth';
+
 const router = require('express').Router()
-const jwt = require('jsonwebtoken')
-const User = require('../model/users')
 const passport = require('passport')
 
+// Login route
 router.route('/login')
-    .post(passport.authenticate('local',{session: false}),async (req, res) => {
-        let user = req.user
+    .post(passport.authenticate('local',{session: false}), async (req, res) => await userLogin(req, res));
 
-        try {
-            let token = jwt.sign({id: user._id}, process.env.JWT_TOKEN)
-            return res.status(200).json({token: token, username: user.username})
-        } catch (e) {
-            return res.status(404).json(e)
-        }
-    })
-
+// Signup route
 router.route('/signup')
-    .post(async (req, res) => {
-        const {username, password} = req.body
-
-        console.log(username, password)
-
-        try {
-            const user = await User.findOne({username: username})
-
-            if (user) {
-                return res.status(401).json({message: "username already exists"})
-            }
-
-            const newUser = new User({
-                username: username,
-                password: password
-            })
-
-            const response = await newUser.save()
-
-            const token = jwt.sign({id: response._id}, process.env.JWT_TOKEN)
-
-            return res.status(201).json({token, username: response.username})
-        } catch (e) {
-            console.log(e)
-            return res.status(404).json(e)
-        }
-    })
+    .post(async (req, res) => await userSignUp(req, res));
 
 module.exports = router
